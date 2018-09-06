1eea7c58600cd214043568899b52aa8b283c98ed
Paulo Henrique Nunes <paulohcode@gmail.com>
2018-09-06 08:36:56 -0300

    Implementado o Template Admin

--- vendor/hcodebr/php-classes/src/PageAdmin.php
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -7,7 +7,7 @@
         public function __construct($opts = array(), $tpl_dir = "/views/admin/")
         {
             
-            parente::__construct($opts, $tpl_dir);
+            parent::__construct($opts, $tpl_dir);
 
         }
 
